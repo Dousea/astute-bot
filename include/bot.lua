600b9aa1f41e25e87702df952146745e1cb0bb4e
Ortimh <orlando.timotius.hutapea@gmail.com>
2018-09-25 21:48:09 +0700

    File names for methods are now enumerated with a built-in function rather than manually entered names; Added comments here and there

--- include/bot.lua
+++ include/bot.lua
@@ -1,8 +1,18 @@
 local bot = {}
 
+-- Every file in 'bots/include/bot/' directory returns a table of functions or
+-- methods to use on the bots, so enumerate the files and add the methods on
+-- each created bot
 do
-	local fromwhere = {'general', 'buy', 'spots', 'noise', 'look', 'engage', 'hook'}
-	local method = {} for _, from in ipairs(fromwhere) do method[#method+1] = require(BOT_DIR .. 'bot/' .. from) end
+	local method = {}
+	
+	for file in io.enumdir(BOT_DIR .. 'bot') do
+		local from = file:match '(.+)%.lua'
+		
+		if from then
+			method[#method + 1] = require(BOT_DIR .. 'bot/' .. from)
+		end
+	end
 	
 	local function createbot(id)
 		local self = {_id = id}
@@ -21,6 +31,8 @@ do
 	end
 end
 
+-- Invokes a method with name of @index of a bot with id @id, and of course
+-- the optional arguments to send on the method
 function bot_invoke(id, index, ...)
 	-- Methods can be invoked if bot is already initialized or @index is 'reset'
 	local self = bot[id] return (index == 'reset' or self._init) and self[index](self, ...) or nil
